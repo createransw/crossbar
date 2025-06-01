230ade24fc3f9588b5cffab3f005d53d7a56e52f
Maksim <Maksim>
2025-06-01 19:28:09 +0300

    better FSM

--- round_robin.sv
+++ round_robin.sv
@@ -8,63 +8,103 @@ module round_robin #(
     input  logic                        rst_n,
 
     input  logic [T_DEST_WIDTH - 1 : 0] number,
-    input  logic                        m_ready,
 
     input  logic [T_DEST_WIDTH - 1 : 0] s_dest_i  [S_DATA_COUNT - 1 : 0],
+    input  logic [S_DATA_COUNT - 1 : 0] s_last_i,
     input  logic [S_DATA_COUNT - 1 : 0] s_valid_i,
-    input  logic                        s_last,
-    output logic [S_DATA_COUNT - 1 : 0] s_ready_o
+
+    input  logic                        m_ready_i,
+
+    output logic                        m_valid_o,
+    output logic                        m_last_o,
+
+    output logic [T_ID___WIDTH - 1 : 0] s_ready_id
 );
 
-typedef enum {READY, BUSY, WAIT} state;
+typedef enum {WAIT, BUSY, LAST} state;
 logic [1 : 0] c_state;
 logic [T_ID___WIDTH - 1 : 0] order; // номер пердыдущего принятого запроса
 
 always_ff @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
-        s_ready_o <= '0;
-        c_state <= WAIT;
+        m_valid_o <= 0;
+        m_last_o <= 0;
+
         order <= 0;
+        c_state <= WAIT;
     end
 end
 
 always_ff @(posedge clk) begin
     case (c_state)
-        READY: begin // slave-устройство готово принять данные 
-            if (~m_ready) begin
-                c_state = WAIT;
-            end else begin
-                s_ready_o <= '0;
-                for (int i = 0; i < S_DATA_COUNT; ++i) begin
-                    logic [T_ID___WIDTH - 1 : 0] id; // номер master-кандидата
-                    logic fit; // действительно ли требуется передача
+        WAIT: begin // slave-устройство ожидает передачу или m_ready_i
+            if (m_ready_i) begin
+                for (int i = 1; i <= S_DATA_COUNT; ++i) begin
+                    logic [T_ID___WIDTH - 1 : 0] id;
+                    logic fit;
 
                     id = (order + i) % S_DATA_COUNT;
                     fit = s_valid_i[id] && (s_dest_i[id] == number);
-                    if (fit) begin
-                        order <= id; // обновляем номер 
-                        s_ready_o[id] <= 1;
-                        c_state <= BUSY;
-                        break;
+
+                    if (fit) begin // найдено подходящее master-устройство
+                        order <= id; // обновление счетчика порядка
+                        m_valid_o <= 1;
+                        s_ready_id <= id;
+                        m_last_o <= s_last_i[id];
+
+                        if (s_last_i[i])
+                            c_state <= LAST;
+                        else
+                            c_state <= BUSY;
                     end
                 end
             end
         end
         BUSY: begin  // идёт передача 
-            if (~m_ready) begin
+            if (~m_ready_i) begin
                 c_state = WAIT;
             end else begin
-                s_ready_o[order] <= 1; // показываем, что данные принимаются
+                m_valid_o <= 1;
+                s_ready_id <= order;
+                m_last_o <= s_last_i[order];
 
-                if (s_last) begin // обрабатывается последний пакет
-                    order <= (order + 1) % S_DATA_COUNT;
-                    c_state <= READY;
-                end
+                if (s_last_i[order])
+                    c_state <= LAST;
             end
         end
-        WAIT: begin // slave-устройство не готово
-            if (m_ready)
-                c_state <= READY;
+        LAST: begin // передаётся последний пакет
+            if (~m_ready_i) begin
+                c_state <= WAIT;
+            end else begin
+                logic instant;
+                instant = 0;
+                for (int i = 1; i <= S_DATA_COUNT; ++i) begin
+                    logic [T_ID___WIDTH - 1 : 0] id;
+                    logic fit;
+
+                    id = (order + i) % S_DATA_COUNT;
+                    fit = s_valid_i[id] && (s_dest_i[id] == number);
+
+                    if (fit) begin // найдено подходящее master-устройство
+                        instant = 1;
+                        order <= id; // обновление счетчика порядка
+                        m_valid_o <= 1;
+                        s_ready_id <= id;
+                        m_last_o <= s_last_i[id];
+
+                        if (s_last_i[i])
+                            c_state <= LAST;
+                        else
+                            c_state <= BUSY;
+                    end
+                end
+                if (~instant) begin
+                    m_valid_o <= 0;
+                    m_last_o <= 0;
+                    order <= (order + 1) % S_DATA_COUNT;
+                    c_state <= WAIT;
+                end
+            end
         end
     endcase
 end

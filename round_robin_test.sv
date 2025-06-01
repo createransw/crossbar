579e1d1b2fd86d04a5e3bae4e59cdccd4b2ce238
Maksim <Maksim>
2025-06-01 19:29:47 +0300

    more complex test for Round_Robin

--- round_robin_test.sv
+++ round_robin_test.sv
@@ -4,12 +4,15 @@ logic clk;
 logic rst_n;
 
 logic [1 : 0] number;
-logic m_ready;
+logic m_ready_i;
 
 logic [1 : 0] s_dest_i [1 : 0];
 logic [1 : 0] s_valid_i;
-logic s_last;
-logic [1 : 0] s_ready_o;
+logic [1 : 0] s_last_i;
+logic s_ready_id;
+
+logic m_valid_o;
+logic m_last_o;
 
 always #5 begin
     clk <= ~clk;
@@ -20,25 +23,29 @@ round_robin uut (
     .rst_n(rst_n),
 
     .number(number),
-    .m_ready(m_ready),
 
     .s_dest_i(s_dest_i),
     .s_valid_i(s_valid_i),
-    .s_last(s_last),
-    .s_ready_o(s_ready_o)
+    .s_last_i(s_last_i),
+
+    .m_ready_i(m_ready_i),
+
+    .m_valid_o(m_valid_o),
+    .m_last_o(m_last_o),
+    .s_ready_id(s_ready_id)
 );
 
 initial begin
-    clk = 0;
+    clk = 1;
     rst_n = 0;
-    #15;
+    #10;
     rst_n = 1;
     number = 0;
-    m_ready = 1;
+    m_ready_i = 1;
     s_dest_i[0] = 0;
     s_dest_i[1] = 0;
     s_valid_i = '0;
-    s_last = 0;
+    s_last_i = '0;
     #10;
     s_valid_i[0] = 1;
     s_dest_i[0] = 0;
@@ -46,27 +53,26 @@ initial begin
     s_valid_i[1] = 1;
     s_dest_i[1] = 0;
     #10;
-    s_last = 1;
+    s_last_i[0] = 1;
     #10;
-    s_last = 0;
     s_valid_i[0] = 0;
     #10;
-    s_last = 1;
+    s_last_i[1] = 1;
     #10;
     s_valid_i[1] = 0;
-    s_last = 0;
+    s_last_i = '0;
     #20;
     s_valid_i[0] = 1;
     s_valid_i[1] = 1;
     #20;
-    s_last = 1;
+    s_last_i[0] = 1;
     #10;
-    s_last = 0;
+    s_last_i[0] = 0;
     s_valid_i[0] = 0;
     #10;
-    s_last = 1;
+    s_last_i[1] = 1;
     #10;
-    s_last = 0;
+    s_last_i[1] = 0;
     s_valid_i[1] = 0;
     #10;
     s_valid_i = '1;

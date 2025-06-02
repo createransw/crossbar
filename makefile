3bf774138af6c3ab8fb72b1a3c337c76b27a73cb
Maksim <Maksim>
2025-06-02 15:16:27 +0300

    autotests

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-all:
+test:
 	~/.intelFPGA/20.1/modelsim_ase/linuxaloem/vsim -c -do run_sim.do
--- stream_xbar_test.sv
+++ stream_xbar_test.sv
@@ -8,11 +8,16 @@ logic s_dest_i [1 : 0];
 logic [1 : 0] s_last_i;
 logic [1 : 0] s_valid_i;
 logic [1 : 0] s_ready_o;
+logic [1 : 0] s_ready_o_d;
 
 logic [3 : 0] m_data_o [1 : 0];
+logic [3 : 0] m_data_o_d [1 : 0];
 logic m_id_o [1 : 0];
+logic m_id_o_d [1 : 0];
 logic [1 : 0] m_last_o;
+logic [1 : 0] m_last_o_d;
 logic [1 : 0] m_valid_o;
+logic [1 : 0] m_valid_o_d;
 logic [1 : 0] m_ready_i;
 
 always #5
@@ -40,51 +45,68 @@ stream_xbar #(
 );
 
 initial begin
+    int fd = $fopen("./tests/vectors.txt", "r");
+    if (fd == 0) $fatal(1, "File not found");
+
     clk = 1;
     rst_n = 0;
     #10;
     rst_n = 1;
     m_ready_i = '1;
-    #10;
-    s_valid_i[0] = 1;
-    s_last_i[0] = 0;
-    s_dest_i[0] = 0;
-    s_data_i[0] = 4'hA;
-
-    s_valid_i[1] = 1;
-    s_last_i[1] = 0;
-    s_dest_i[1] = 0;
-    s_data_i[1] = 4'hC;
-
-    #10;
-    s_last_i[0] = 1;
-    s_data_i[0] = 4'hB;
-    #10;
-    s_valid_i[0] = 0;
-    #10;
-
-    s_last_i[1] = 0;
-    s_dest_i[1] = 0;
-    s_last_i[1] = 1;
-    s_data_i[1] = 4'hD;
-    #10;
-    s_valid_i[0] = 1;
-    s_last_i[0] = 0;
-    s_dest_i[0] = 1;
-    s_data_i[0] = 4'hE;
-
-    s_last_i[1] = 0;
-    s_data_i[1] = 8;
-    #10;
-    s_data_i[0] = 4'hF;
-    s_last_i[0] = 1;
-
-    s_last_i[1] = 1;
-    s_data_i[1] = 9;
-    #10;
-    s_valid_i = '0;
-    #50;
    
+    for (int i = 0; i < 7; ++i) begin
+        $fscanf(
+            fd,
+            "%x %x %x %x %b %b %b    %b %x %x %x %x %b %b",
+            s_data_i[1],
+            s_data_i[0],
+            s_dest_i[1],
+            s_dest_i[0],
+            s_last_i,
+            s_valid_i,
+            m_ready_i,
+
+            s_ready_o_d,
+            m_data_o_d[1],
+            m_data_o_d[0],
+            m_id_o_d[1],
+            m_id_o_d[0],
+            m_last_o_d,
+            m_valid_o_d
+        );
+        #10;
+
+        if (m_valid_o_d != m_valid_o)
+            $error("Mismatch in m_valid_o");
+
+        if (m_valid_o[0]) begin
+            if (m_data_o_d[0] != m_data_o[0])
+                $error("Mismatch in m_data_o[0]");
+            if (m_id_o_d[0] != m_id_o[0])
+                $error("Mismatch in m_id_o[0]");
+            if (m_last_o_d[0] != m_last_o[0])
+                $error("Mismatch in m_last_o[0]");
+        end
+        if (m_valid_o[1]) begin
+            if (m_data_o_d[1] != m_data_o[1])
+                $error("Mismatch in m_data_o[1]");
+            if (m_id_o_d[1] != m_id_o[1])
+                $error("Mismatch in m_id_o[1]");
+            if (m_last_o_d[1] != m_last_o[1])
+                $error("Mismatch in m_last_o[1]");
+        end
+
+        if (s_valid_i[0]) begin
+            if (s_ready_o_d[0] != s_ready_o[0])
+                $error("Mismatch in s_ready_o[0]");
+        end
+        if (s_valid_i[1]) begin
+            if (s_ready_o_d[1] != s_ready_o[1])
+                $error("Mismatch in s_ready_o[1]");
+        end
+    end
+
+    $fclose(fd);
     $finish;
 end
 
